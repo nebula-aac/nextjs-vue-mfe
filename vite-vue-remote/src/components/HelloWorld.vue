0f9123e12e8cadc29ea4b415deb1dcb389196e30
Antonette Caldwell <acald.nebula@gmail.com>
2023-06-05 16:35:38 -0500

    configured mod fed, and updated component

--- vite-vue-remote/src/components/HelloWorld.vue
+++ vite-vue-remote/src/components/HelloWorld.vue
@@ -1,34 +1,20 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { defineAsyncComponent, ref } from 'vue'
 
-defineProps<{ msg: string }>()
+const isLoadingComponent = ref(true)
 
-const count = ref(0)
+const HelloWorld = defineAsyncComponent(() => import('vue_remote/HelloWorld.vue')
+  .finally(() => {
+    isLoadingComponent.value = false
+  })
+)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div class="home">
+    <div v-if="isLoadingComponent">Loading HelloWorld.vue</div>
+    <HelloWorld msg="Welcome to Vue.js" />
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
--- vite-vue-remote/vite.config.ts
+++ vite-vue-remote/vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import federation from '@originjs/vite-plugin-federation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  cacheDir: 'node_modules/.cacheDir',
+  plugins: [
+    vue(),
+    federation({
+      name: 'vite-vue',
+      filename: 'remoteEntry.js',
+      remotes: {
+        'vue_remote': 'http://localhost:5001/remoteEntry.js',
+        shared: ['vue']
+      }
+    })
+  ],
+  build: {
+    minify: false,
+    target: 'esnext'
+  }
 })
