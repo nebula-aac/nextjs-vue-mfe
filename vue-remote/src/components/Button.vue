756dc8e31fe356d48cd064d8d58476fac111219d
Antonette Caldwell <acald.nebula@gmail.com>
2023-06-05 16:35:54 -0500

    configured mod fed, and updated button

---
+++ vue-remote/src/components/Button.vue
@@ -0,0 +1,3 @@
+<template>
+  <button type="button">Vue Remote Button</button>
+</template>
--- vue-remote/vue.config.js
+++ vue-remote/vue.config.js
@@ -1,4 +1,49 @@
 const { defineConfig } = require("@vue/cli-service");
+const webpack = require("webpack");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  pages: {
+    index: {
+      entry: "./src/main.ts",
+    },
+  },
+  publicPath: "auto",
+  configureWebpack: {
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          defaultVendors: {
+            name: "chunk-vendors",
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10,
+            chunks: "async",
+            reuseExistingChunk: true,
+          },
+          common: {
+            name: "chunk-common",
+            minChunks: 2,
+            priority: -20,
+            chunks: "async",
+            reuseExistingChunk: true,
+          },
+        },
+      },
+    },
+    plugins: [
+      new webpack.container.ModuleFederationPlugin({
+        name: "vue_remote",
+        filename: "remoteEntry.js",
+        exposes: {
+          "./HelloWorld.vue": "./src/components/HelloWorld.vue",
+          "./Button.vue": "./src/components/Button.vue",
+        },
+        shared: {
+          vue: {
+            singleton: true,
+          },
+        },
+      }),
+    ],
+  },
 });
